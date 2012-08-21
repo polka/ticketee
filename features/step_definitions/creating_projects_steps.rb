16c6e1a8f64cd3267b716d9032d7123d6258cde3
polka volka <real.polka@googlemail.com>
2012-08-21 18:36:59 +0200

    scaffold projects. cucumber create_projects.feature pass.

---
+++ features/step_definitions/creating_projects_steps.rb
@@ -0,0 +1,21 @@
+Given /^I am on the homepage$/ do
+	visit root_path
+end
+
+When /^I follow "(.*?)"$/ do |arg1|
+	click_on arg1
+end
+
+
+When /^I fill in "(.*?)" with "(.*?)"$/ do |arg1, arg2|
+	fill_in arg1, :with => arg2
+end
+
+When /^I press "(.*?)"$/ do |arg1|
+	click_on arg1
+end
+
+Then /^I should see "(.*?)"$/ do |arg1|
+  page.has_content? arg1
+end
+
